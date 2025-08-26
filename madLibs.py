4c6f6469069242640e2f09af042fded92025fb91
sko101 <sko101@stge.org.uk>
2025-08-26 09:24:31 +0000

    started on madLibs

---
+++ madLibs.py
@@ -0,0 +1,9 @@
+name = input("Enter a name: ")
+age = input("Enter an age: ")
+food = input("Enter a food: ")
+place = input("Enter a place: ")
+pronoun = input("Enter a pronoun (e.g. she/he): ")
+
+print(name + " is " + age + " years old and loves eating " + food + ".")
+print(pronoun
+     " lives in " + place + ".")
