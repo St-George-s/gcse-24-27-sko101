0b3752cdaa730967d9a0a270d9438520f2b3ad8b
sko101 <sko101@stge.org.uk>
2025-10-03 12:42:03 +0000

    homework

---
+++ comparisonAndArithmeticOperators.py
@@ -0,0 +1,69 @@
+# # Question 1 
+# # Write a Python program to check if two numbers are equal.
+# # Ask the user to input two numbers and print whether they are equal or not. 
+
+# usersNumber1 = int(input("Please enter a number: "))
+# usersNumber2 = int(input("Please enter a second number: "))
+# if usersNumber1 == usersNumber2:
+#     print("Your two numbers are equal. ")
+# if usersNumber1 != usersNumber2:
+#     print("Your two numbers are not equal. ")
+
+
+# # Question 2 
+# # Write a Python program to find the larger of two numbers.
+# # Ask the user to input two numbers and print the larger number. 
+
+# usersNumberOne = int(input("Please enter a number: "))
+# usersNumberTwo = int(input("Please enter a second number: "))
+# if usersNumberOne > usersNumberTwo:
+#     print("Your first number is larger than your second. (" + str(usersNumberOne) + ")")
+# if usersNumberOne < usersNumberTwo:
+#     print("Your second number is larger than your first. (" + str(usersNumberTwo) + ")")
+# if usersNumberOne == usersNumberTwo:
+#     print("Your two numbers are equal. ")
+
+
+# # Question 3 
+# # Write a Python program to calculate the remainder when one number is divided by another.
+# # Ask the user for two numbers and print the remainder. 
+
+# usersNumber1One = int(input("Please enter a number: "))
+# usersNumber2Two = int(input("Please enter a second number: "))
+# print("The remainder of your two numbers is: " + str(usersNumber1One % usersNumber2Two))
+
+
+# # Question 4 
+# # Write a Python program to check if a number is positive, negative, or zero.
+# # Ask the user for a number and print the result. 
+
+# usersNumberSingle = int(input("Please enter a number: "))
+# evenOrOdd = int(usersNumberSingle % 2)
+# if usersNumberSingle == 0:
+#     print("Your number is zero. ")
+# elif evenOrOdd == 0:
+#     print("Your number is even. ")
+# elif evenOrOdd == 1:
+#     print("Your number is odd. ")
+
+
+# # Question 5 
+# # Write a Python program to calculate the area of a rectangle.
+# # Ask the user to input the length and width of the rectangle and print the area.
+
+# usersNumberLength = int(input("Please enter the length: "))
+# usersNumberWidth = int(input("Please enter the width: "))
+# print("Your rectangle's area is " + str(usersNumberLength * usersNumberWidth) + ".")
+
+
+# # Question 6 
+# # Write a Python program to check if one number is a multiple of another.
+# # Ask the user to input two numbers and print whether the first number is a multiple of the second. 
+
+# usersNumberOne1 = int(input("Please enter a number: "))
+# usersNumberTwo2 = int(input("Please enter a second number: "))
+# isAMultiple = int(usersNumberTwo2 % usersNumberOne1)
+# if isAMultiple == 0:
+#     print("Your first number is a multiple of the second. ")
+# else:
+#     print("Your first number is not a multiple of the second. ")
