0d0791032cbf990eebe18c02a0afc0f919fe7890
sko101 <sko101@stge.org.uk>
2026-01-08 21:00:12 +0000

    08/01/2026

--- fileSize.py
+++ fileSize.py
@@ -1,10 +1,55 @@
+# print("To find out the file size, please: ")
+# width = int(input("Enter your image width: "))
+# height = int(input("Enter your image height: "))
+# colourDepth = int(input("Enter your image colour depth: "))
+# fileSize = width * height * colourDepth
+# fileSizeInBytes = fileSize / 8
+# if fileSizeInBytes < 1:
+#     print("Your estimated file size is " + str(fileSize) + " in bits.")
+# else:
+#     print("Your estimated file size is " + str(fileSizeInBytes) + " in bytes.")
+
+
+
+
+# wantToContinue = True
+# stayInBits = False
+# bbkmgtpCounter = 0
+# bbkmgtp = ["bits", "bytes", "kilobytes", "megabytes", "gigabytes", "terabytes", "petabytes"]
+# while wantToContinue == True:
+#     print("To find out the file size, please: ")
+#     width = int(input("Enter your image width: "))
+#     height = int(input("Enter your image height: "))
+#     colourDepth = int(input("Enter your image colour depth: "))
+#     fileSize = width * height * colourDepth
+#     if fileSize > 1:
+#         fileSize = fileSize / 8
+#         bbkmgtpCounter = 1
+#         if fileSize < 1:
+#             fileSize = fileSize * 8
+#             bbkmgtpCounter = bbkmgtpCounter - 1
+#     while fileSize > 1:
+#         fileSize = fileSize * 1000
+#         bbkmgtpCounter = bbkmgtpCounter + 1
+#     if fileSize < 1:
+#             fileSize = fileSize * 1000
+#             bbkmgtpCounter = bbkmgtpCounter - 1
+#     print("Your estimated file size is " + str(fileSize) + " in " + bbkmgtp[bbkmgtpCounter] + ". ")
+
+
+
 print("To find out the file size, please: ")
 width = int(input("Enter your image width: "))
 height = int(input("Enter your image height: "))
 colourDepth = int(input("Enter your image colour depth: "))
 fileSize = width * height * colourDepth
-fileSizeInBytes = fileSize / 8
-if fileSizeInBytes < 1:
+fileSize2 = fileSize / 8
+if fileSize2 < 1:
     print("Your estimated file size is " + str(fileSize) + " in bits.")
 else:
-    print("Your estimated file size is " + str(fileSizeInBytes) + " in bytes.")
+    while fileSize2 > 1:
+        fileSize2 = fileSize2 * 1000    
+print("Your estimated file size is " + str(fileSize2) + " in bytes.")
+
+
+
