d9d178317e1c343cec53149c75b8c2c61ff703b8
sko101 <sko101@stge.org.uk>
2026-01-09 15:30:34 +0000

    doneeeeee

--- fileSize.py
+++ fileSize.py
@@ -37,19 +37,23 @@
 #     print("Your estimated file size is " + str(fileSize) + " in " + bbkmgtp[bbkmgtpCounter] + ". ")
 
 
-
+# Works
+bbkmgtpCounter = 1
+bbkmgtp = ["bits", "bytes", "kilobytes", "megabytes", "gigabytes", "terabytes", "petabytes"]
 print("To find out the file size, please: ")
 width = int(input("Enter your image width: "))
 height = int(input("Enter your image height: "))
 colourDepth = int(input("Enter your image colour depth: "))
 fileSize = width * height * colourDepth
-fileSize2 = fileSize / 8
-if fileSize2 < 1:
-    print("Your estimated file size is " + str(fileSize) + " in bits.")
+fileSize = fileSize / 8
+if fileSize < 1:
+    print("Your estimated file size is " + str(fileSize * 8) + " in bits.")
 else:
-    while fileSize2 > 1:
-        fileSize2 = fileSize2 * 1000    
-print("Your estimated file size is " + str(fileSize2) + " in bytes.")
+    while fileSize >= 10000:
+            fileSize = fileSize / 1024   
+            bbkmgtpCounter = bbkmgtpCounter + 1  
+    print("Your estimated file size is " + str(fileSize) + " in " + str(bbkmgtp[bbkmgtpCounter] + ". "))
 
 
 
+            
