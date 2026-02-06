6ac6f51237e86ed0924a3929149a4f643a6d30a4
sko101 <sko101@stge.org.uk>
2026-02-06 15:38:42 +0000

    06/02/2026

--- connections.py
+++ connections.py
@@ -1,1 +1,47 @@
-print("HEllo")
+# Connections game.
+
+
+# Functions:
+
+# Sets the words to the example words.
+def useExampleSet():
+    set1 = ["MOVE", "REACH", "SWAY", "TOUCH"]
+    set2 = ["BINGO", "CORRECT", "DING", "SWAY"]
+    set3 = ["CHANGE", "GREEN", "PAPER", "SCRATCH"]
+    set4 = ["CHIP", "PHONE", "SCOPE", "WAVE"]
+    return set1, set2, set3, set4
+
+# Generates an order for the words to be displayed in.
+def generateOrder():
+    wordsInOrder = ""
+    for counter in range(16):
+        numberForSetChosen = random.randint(0,3)
+        numberForWordInSetChosen = random.randint(0,3)
+        if numberForSetChosen == 1:
+            setChosen = set1
+        elif numberForSetChosen == 2:
+            setChosen = set2
+        elif numberForSetChosen == 3:
+            setChosen = set3
+        else:
+            setChosen = set4
+        setChosen[numberForWordInSetChosen]
+        print(setChosen[numberForWordInSetChosen])
+
+
+# Main code:
+import random
+
+
+
+# Uses the example set.
+set1, set2, set3, set4 = useExampleSet()
+
+generateOrder()
+
+
+
+
+
+
+# affect, you got it, slang for money, objects with the prefix "micro-"
