839237667e5a5d4e677b98b429deb543f1677a93
sko101 <sko101@stge.org.uk>
2026-02-06 16:47:33 +0000

    hdoiawhda

--- wordle.py
+++ wordle.py
@@ -1,12 +1,40 @@
 exampleWord = "swoop"
 
 
+
+# Functions:
+def keyboard
+
+
+
+
+
+
+
+# Main code:
+
+
+
+
+
+
+
+
+
 keyboard = "q w e r t y u i o p"
-keyboardLine2 = "a s d f g h j k l"
-keyboardLine3 = "z x c v b n m"
+keyboardLine2 = " a s d f g h j k l"
+keyboardLine3 = "   z x c v b n m"
 
 letterGuessed = input("guess: ")
 for counter in range(len(keyboard)):
     if letterGuessed == keyboard[counter]:
         keyboard = keyboard[0:counter] + " " + keyboard[counter+1:]
+for counter in range(len(keyboardLine2)):
+    if letterGuessed == keyboardLine2[counter]:
+        keyboardLine2 = keyboardLine2[0:counter] + " " + keyboardLine2[counter+1:]
+for counter in range(len(keyboardLine3)):
+    if letterGuessed == keyboardLine3[counter]:
+        keyboardLine3 = keyboardLine3[0:counter] + " " + keyboardLine3[counter+1:]
 print(keyboard)
+print(keyboardLine2)
+print(keyboardLine3)
